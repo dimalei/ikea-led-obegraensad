52e97357757ebe8b6188dcec9764a3790c13334d
dimalei <dima.leigr@gmail.com>
2023-12-20 16:43:08 +0100

    fade animation

--- include/plugins/OffPlugin.h
+++ include/plugins/OffPlugin.h
@@ -2,9 +2,14 @@
 
 #include "PluginManager.h"
 
-class OffPlugin : public Plugin {
+class OffPlugin : public Plugin 
+{
+private:
+    int fadeBrightness;
+    int previousBrighntess;
 public:
     void setup() override;
     void loop() override;
     const char* getName() const override;
+    void teardown() override;
 };
--- src/plugins/OffPlugin.cpp
+++ src/plugins/OffPlugin.cpp
@@ -3,11 +3,26 @@
 
 void OffPlugin::setup() {
     // Setup logic for your plugin
-    Screen.clear();
+    fadeBrightness = previousBrighntess = Screen.getCurrentBrightness();
+    //Screen.clear();
 }
 
 void OffPlugin::loop() {
     // Loop logic for your plugin
+    if(fadeBrightness > 1)
+    {
+        fadeBrightness--;
+        Screen.setBrightness(fadeBrightness);
+    } else if (fadeBrightness == 1)
+    {
+        Screen.clear();
+    }
+    
+}
+
+void OffPlugin::teardown() {
+  // code if plugin gets deactivated
+  Screen.setBrightness(previousBrighntess);
 }
 
 const char* OffPlugin::getName() const {
